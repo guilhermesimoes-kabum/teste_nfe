15b110c33883ec9d55eab8bcf203ab506d852301
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-04-13 13:53:48 -0300

    feat: initial SEFAZ module

--- src/V1/entities/SEFAZ/NFE.ts
+++ src/V1/entities/SEFAZ/NFE.ts
@@ -1,5 +1,32 @@
+import {ParametersToIssuer} from "src/V1/shared/parametersToIssuer.interface";
+import {Taxation} from "../taxation";
 
 
 export class NFE {
+	constructor(
+		private taxation: Taxation,
+		private parameterToIssuer : ParametersToIssuer
+	){
+		this.generateMandatoryGroups();
+	}; 
 
+	generateMandatoryGroups() {
+
+	}
+
+	det() {
+
+	}
+
+	ide() {
+
+	}
+
+	emit() {
+
+	}
+
+	total() {
+
+	}
 }
