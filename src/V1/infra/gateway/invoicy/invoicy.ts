23709a4b93c84d1cef8b981ff304ec9d0ecc1e18
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-04 10:40:56 -0300

    refactor: refactoring invoicy gateway to use adapter

--- src/V1/infra/gateway/invoicy/invoicy.ts
+++ src/V1/infra/gateway/invoicy/invoicy.ts
@@ -1,20 +1,20 @@
+import {InvoicyAdapter} from "src/V1/domain/adapter/invoicyAdapter";
+import {NFE} from "src/V1/domain/entities/SEFAZ/NFE";
 import {ResponseIssuer} from "../adapter/responseIssuer";
 import {IssuerInterface} from "../shared/issuer.interface";
 import {ResponseInvoicyStub} from "./responseInvoicyStub";
 
 export class Invoicy implements IssuerInterface {
-	async sendInvoicy(): Promise<ResponseIssuer> {
-		const resposta_invoicy = ResponseInvoicyStub.respota();	
-		const resposta = new ResponseIssuer(); 
-		resposta.xml_link = resposta_invoicy[0].DocXMLDownload;
-		resposta.pdf_link = resposta_invoicy[0].DocPDFDownload;
-		resposta.xml_base64 = resposta_invoicy[0].DocXMLBase64;
-		resposta.pdf_base64 = resposta_invoicy[0].DocXMLBase64;
-		resposta.statusCode = resposta_invoicy[0].Situacao.SitCodigo;
-		resposta.mensagem = resposta_invoicy[0].Situacao.SitDescricao;
+	async sendInvoicy(invoice : NFE): Promise<ResponseIssuer> {
+
+		const responseAdapter = new InvoicyAdapter();
+		const invoiceInvoicy = responseAdapter.input(invoice);
+
+		const responseInvoicy = ResponseInvoicyStub.respota();
+		const response = responseAdapter.output(responseInvoicy);
 
 		return new Promise(function (resolve){ 
-			resolve(resposta);
+			resolve(response);
 		});
 	}
 }
