1e2cbd278fd5d4f6c84509e12bf0346c6092be99
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-04 17:43:03 -0300

    feat: create interface to taxation

---
+++ src/V1/domain/interface/TaxationInterface.ts
@@ -0,0 +1,35 @@
+export interface ICMS {
+	PERC_RED_BC : number;
+	CODIGO : string;
+	ALIQ : number;
+	FCP : number;
+}
+
+export interface PIS {
+	PERC_RED_BC : number;
+	DISP_LEG : string;
+	ALIQ : number;
+	CODIGO : string;
+}
+
+export interface COFINS {
+	PERC_RED_BC : number;
+	DISP_LEG : string;
+	ALIQ : number;
+	CODIGO : string;
+}
+
+export interface ICMS_UFDEST {
+	PERC_RED_BC : number;
+	PERC_PART_DEST : number;
+	CODIGO : string;
+	ALIQ : number;
+	FCP : number;
+}
+
+export interface TaxationInterface {
+	getDadosICMS() : ICMS;
+	getDadosPIS() : PIS;
+	getDadosCOFINS() : COFINS;
+	getDadosICMS_UFDEST() : ICMS_UFDEST;
+}
