5cbe9857badaffef8c041d41836de91c46d1d0e4
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-05 11:57:33 -0300

    feat: treated a taxation

--- src/V1/usecases/nfesale.service.ts
+++ src/V1/usecases/nfesale.service.ts
@@ -1,17 +1,33 @@
 import {ResponseIssuer} from "../adapter/responseIssuer";
 import {NFE} from "../domain/entities/SEFAZ/NFE";
+import {typeTaxation} from "../domain/interface/parametersToIssuer.interface";
 import {TaxationInterface} from "../domain/interface/TaxationInterface";
+import {TaxationIssuerKabumOrder} from "../infra/repositories/taxationIssuerKabumOrder";
 import {IssuerInterface} from "../shared/issuer.interface";
 import {ParametersToIssuer} from "../shared/parametersToIssuer.interface";
 
 export class NFeSaleService {
 	constructor(
-		private issuer : IssuerInterface,
-		private taxation : TaxationInterface
+		private issuer : IssuerInterface
 	) {}
 
-	async issueSalesInvoicy(informationOrderToInvoicy : ParametersToIssuer) : Promise<ResponseIssuer> {
-		const nfe = new NFE(informationOrderToInvoicy);
-		return this.issuer.sendInvoicy();
+	async issueSalesInvoicy(informationOrderToInvoice : ParametersToIssuer) : Promise<ResponseIssuer> {
+		let taxObj = informationOrderToInvoice.taxation;
+		if(taxObj == null) {
+			const tax = informationOrderToInvoice.typeTaxation;
+			if(tax == null) {
+				throw new Error();
+			}
+			taxObj = this.getTaxObj(tax);
+		}
+
+		const nfe = new NFE(informationOrderToInvoice, taxObj);
+		return this.issuer.sendInvoicy(nfe);
+	}
+
+	getTaxObj(tax : typeTaxation) : TaxationInterface {
+		if(tax == 0) {
+			return new TaxationIssuerKabumOrder() 
+		}
 	}
 }
