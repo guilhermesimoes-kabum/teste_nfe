52bf117f916061e50f7ef47c689621c9c348544b
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-05 13:18:44 -0300

    feat: simulation of the sendDocument

--- src/V1/infra/gateway/invoicy/invoicy.ts
+++ src/V1/infra/gateway/invoicy/invoicy.ts
@@ -2,6 +2,8 @@ import {InvoicyAdapter} from "src/V1/domain/adapter/invoicyAdapter";
 import {NFE} from "src/V1/domain/entities/SEFAZ/NFE";
 import {ResponseIssuer} from "../adapter/responseIssuer";
 import {IssuerInterface} from "../shared/issuer.interface";
+import {InvoiceInvoicy} from "./invoiceInvoicy";
+import {InvoicyResponse} from "./InvoicyResponse";
 import {ResponseInvoicyStub} from "./responseInvoicyStub";
 
 export class Invoicy implements IssuerInterface {
@@ -10,11 +12,23 @@ export class Invoicy implements IssuerInterface {
 		const responseAdapter = new InvoicyAdapter();
 		const invoiceInvoicy = responseAdapter.input(invoice);
 
-		const responseInvoicy = ResponseInvoicyStub.respota();
+		const responseInvoicy = this.sendDocument(invoiceInvoicy);
 		const response = responseAdapter.output(responseInvoicy);
 
 		return new Promise(function (resolve){ 
 			resolve(response);
 		});
+	}
+
+	sendDocument(invoiceInvoicy : InvoiceInvoicy) : InvoicyResponse {
+		return ResponseInvoicyStub.respota();
+	}
+
+	getToken() : string {
+		return "Bearer 111111";
+	}
+
+	generateToken() {
+
 	}
 }
