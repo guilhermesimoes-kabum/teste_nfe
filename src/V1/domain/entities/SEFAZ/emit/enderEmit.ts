8c22e61a4bef4c9a63c25239f0d88c913e7ba6d4
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-06 16:33:53 -0300

    create a file the field enderEmit

---
+++ src/V1/domain/entities/SEFAZ/emit/enderEmit.ts
@@ -0,0 +1,26 @@
+import {ParametersToIssuer} from "../../../interface/parametersToIssuer.interface";
+
+export class enderEmit {
+	constructor(private parameterToIssuer : ParametersToIssuer) {
+		this.buildEmit();
+	}
+ 
+	private _xLgr : xLgr;
+	private _nro : nro;
+	private _xCpl : xCpl;
+	private _xBairro : xBairro;
+	private _cMun: cMun;
+	private _xMun: xMun;
+	private _UF: UF;
+	private _CEP: CEP;
+	private _cPais: cPais;
+	private _xPais: xPais;
+	private _fone: fone;
+	private _IE: IE;
+	private _IEST: IEST;
+
+	buildEmit() {
+	
+	}
+}
+
