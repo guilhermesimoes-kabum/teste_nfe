e4d5c9ece6f566fb452a080dd682d44d892fc073
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-06 14:40:52 -0300

    delete files created by typeorm

--- database.provider.ts
+++ database.provider.ts
@@ -1,19 +1,17 @@
-import {createConnection} from "typeorm";
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import {KbNotasfiscaisAliquotaICMSestaado} from "./src/V1/infra/repositories/entities/KbNotasfiscaisAliquotaICMSestado";
 
-export const databaseProviders = [
-  {
-    provide: 'DATABASE_CONNECTION',
-    useFactory: async () => await createConnection({
-      type: 'mysql',
-      host: 'gkt-aurora-ecom-qa-instance-cluster.cluster-chxpadil0npm.us-east-1.rds.amazonaws.com',
-      port: 3306,
-      username: 'guilherme.simoes',
-      password: '74okuj79hyyyY',
-      database: 'gk',
-      entities: [
-          __dirname + '/../**/*Entity{.ts,.js}',
-      ],
-      synchronize: true,
-    }),
-  },
-];
+export const configMysqlQA = new DataSource({
+    type: "mysql",
+    host: "gkt-aurora-ecom-qa-instance-cluster.cluster-chxpadil0npm.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    username: "guilherme.simoes",
+    password: "74okuj79hyyyY",
+    database: "gk",
+    synchronize: true,
+    logging: false,
+    entities: [KbNotasfiscaisAliquotaICMSestaado],
+    migrations: [],
+    subscribers: [],
+})
