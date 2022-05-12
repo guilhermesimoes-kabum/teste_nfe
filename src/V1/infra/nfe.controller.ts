f9db6592debbd27e32eed332287ab906a3478020
Guilherme Giacomo Simoes <guilherme.simoes@kabum.com.br>
2022-05-12 16:00:43 -0300

    refactor: remove uneccessary import

--- src/V1/infra/nfe.controller.ts
+++ src/V1/infra/nfe.controller.ts
@@ -2,7 +2,6 @@ import {Body, Controller, HttpException, HttpStatus, Post} from "@nestjs/common"
 import {ParametersToIssuer} from "../domain/interface/parametersToIssuer.interface";
 import {ResponseIssuer} from "../domain/interface/responseIssuer";
 import {Facade} from "./facade";
-import {InCluster} from "./lib/InCluster";
 
 @Controller('nfe')
 export class NFeController {
